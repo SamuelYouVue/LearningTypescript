295cb40797fb29d709627f3946ed087ef20b626d
Samuel You <SamuelYou2021@outlook.com>
2021-03-06 18:15:26 -0500

    up to uglify

---
+++ ch02/src/ts/main.ts
@@ -0,0 +1,8 @@
+import { sayHello } from "./greet";
+function showHello(divName: string, name: string) {
+    const elt = document.getElementById(divName);
+    if (elt) {
+        elt.innerText = sayHello(name);
+    }
+}
+showHello("greeting", "TypeScript");
